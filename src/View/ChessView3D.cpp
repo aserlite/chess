957acaff8ffc6506fde029f903152500e80a5b68
akingram <guilland.killian@gmail.com>
2026-04-16 23:52:05 +0200

    full hd mode !!!

--- src/View/ChessView3D.cpp
+++ src/View/ChessView3D.cpp
@@ -5,10 +5,10 @@
 #include "3D/Framebuffer.hpp"
 #include "3D/MousePicker.hpp"
 #include "3D/PieceRenderer.hpp"
+#include "3D/PovCamera.hpp"
 #include "3D/SceneRenderer.hpp"
 #include "3D/Skybox.hpp"
 #include "3D/TrackballCamera.hpp"
-#include "3D/PovCamera.hpp"
 #include "Logic/Game.hpp"
 
 ChessView3D::ChessView3D(OnMoveRequestedCb onMoveRequested)
@@ -42,9 +42,7 @@ void ChessView3D::draw(ChessGame& game, ViewContext& ctx)
     ImGui::SetNextWindowPos(viewport->WorkPos);
     ImGui::SetNextWindowSize(viewport->WorkSize);
 
-    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | 
-                                    ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoBringToFrontOnFocus | 
-                                    ImGuiWindowFlags_NoNavFocus;
+    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
 
     ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
     ImGui::Begin("Vue 3D", nullptr, window_flags);
@@ -60,37 +58,39 @@ void ChessView3D::draw(ChessGame& game, ViewContext& ctx)
 
     drawMenuBar();
 
-    // 1. Calculate size and layout
-    ImVec2 size = ImGui::GetContentRegionAvail();
-    if (size.x <= 0.0f) size.x = 600.0f;
-    if (size.y <= 0.0f) size.y = 600.0f;
+    // 1. Calculate logical and physical sizes for High-DPI (Retina) support
+    ImVec2 logicalSize = ImGui::GetContentRegionAvail();
+    if (logicalSize.x <= 0.0f)
+        logicalSize.x = 600.0f;
+    if (logicalSize.y <= 0.0f)
+        logicalSize.y = 600.0f;
 
-    m_framebuffer->resizeIfNeeded(static_cast<int>(size.x), static_cast<int>(size.y));
+    ImVec2 fbScale      = ImGui::GetIO().DisplayFramebufferScale;
+    ImVec2 physicalSize = ImVec2(logicalSize.x * fbScale.x, logicalSize.y * fbScale.y);
 
-    // 2. Hover Calculation (Before rendering so we can pass it down)
-    const ImVec2 pos = ImGui::GetCursorScreenPos();
+    m_framebuffer->resizeIfNeeded(static_cast<int>(physicalSize.x), static_cast<int>(physicalSize.y));
+
+    // 2. Hover Calculation (Uses logical coordinates)
+    const ImVec2            pos = ImGui::GetCursorScreenPos();
     std::optional<Position> hoveredPos;
 
-    if (ImGui::IsMouseHoveringRect(pos, ImVec2(pos.x + size.x, pos.y + size.y)))
+    if (ImGui::IsMouseHoveringRect(pos, ImVec2(pos.x + logicalSize.x, pos.y + logicalSize.y)))
     {
         ImVec2    mousePos(ImGui::GetMousePos());
         glm::vec2 localMousePos(mousePos.x - pos.x, mousePos.y - pos.y);
-        glm::vec2 windowSize(size.x, size.y);
+        glm::vec2 windowSizeLogical(logicalSize.x, logicalSize.y);
 
-        hoveredPos = m_mousePicker->getBoardPosition(localMousePos, windowSize, 
-                                                    m_activeCamera->getViewMatrix(), 
-                                                    m_activeCamera->getProjectionMatrix(size.x, size.y));
+        hoveredPos = m_mousePicker->getBoardPosition(localMousePos, windowSizeLogical, m_activeCamera->getViewMatrix(), m_activeCamera->getProjectionMatrix(logicalSize.x, logicalSize.y));
     }
 
     // 3. Render
-    renderPipeline(game, ctx, size, hoveredPos);
+    renderPipeline(game, ctx, logicalSize, hoveredPos);
 
     // 4. Interaction
-    ImGui::Image(reinterpret_cast<void*>(static_cast<intptr_t>(m_framebuffer->getTextureId())), 
-                 size, ImVec2(0, 1), ImVec2(1, 0));
+    ImGui::Image(reinterpret_cast<void*>(static_cast<intptr_t>(m_framebuffer->getTextureId())), logicalSize, ImVec2(0, 1), ImVec2(1, 0));
 
     ImGui::SetCursorScreenPos(pos);
-    ImGui::InvisibleButton("##3DViewBtn", size);
+    ImGui::InvisibleButton("##3DViewBtn", logicalSize);
 
     handleInput(game, hoveredPos, ctx);
 
@@ -127,23 +127,20 @@ void ChessView3D::renderPipeline(const ChessGame& game, const ViewContext& ctx, 
     // Bind and Draw
     GLint prev_fbo;
     glGetIntegerv(GL_FRAMEBUFFER_BINDING, &prev_fbo);
-    
+
     m_framebuffer->bind();
     m_framebuffer->clear(0.2f, 0.3f, 0.3f);
-    
+
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    m_sceneRenderer->drawScene(game, ctx, 
-                              m_activeCamera->getViewMatrix(), 
-                              m_activeCamera->getProjectionMatrix(size.x, size.y), 
-                              hoveredPos, m_visualState);
+    m_sceneRenderer->drawScene(game, ctx, m_activeCamera->getViewMatrix(), m_activeCamera->getProjectionMatrix(size.x, size.y), hoveredPos, m_visualState);
 
     glDisable(GL_DEPTH_TEST);
     glDisable(GL_BLEND);
     m_framebuffer->unbind();
-    
+
     glBindFramebuffer(GL_FRAMEBUFFER, prev_fbo);
 }
 
@@ -161,14 +158,14 @@ void ChessView3D::handleInput(const ChessGame& game, const std::optional<Positio
     if (ImGui::IsItemHovered())
     {
         m_activeCamera->processMouseScroll(ImGui::GetIO().MouseWheel);
-        
+
         if (ImGui::IsMouseClicked(ImGuiMouseButton_Right))
             ctx.selectedPos = {-1, -1};
 
         if (ImGui::IsMouseClicked(ImGuiMouseButton_Left) && hoveredPos)
         {
-            const auto& board = game.getBoard();
-            const Piece& p = board.getPiece(hoveredPos->x, hoveredPos->y);
+            const auto&  board = game.getBoard();
+            const Piece& p     = board.getPiece(hoveredPos->x, hoveredPos->y);
 
             if (ctx.selectedPos.x == -1)
             {
@@ -185,7 +182,7 @@ void ChessView3D::handleInput(const ChessGame& game, const std::optional<Positio
                 {
                     if (m_onMoveRequested)
                         m_onMoveRequested(ctx.selectedPos, *hoveredPos);
-                    
+
                     // UX: Re-select if we clicked a friendly piece, otherwise clear
                     if (!p.isEmpty() && p.color == game.getCurrentTurn())
                         ctx.selectedPos = *hoveredPos;
